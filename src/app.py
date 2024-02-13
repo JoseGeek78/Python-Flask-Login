7a83a0f5904ac7e9a237835bcbc1866240ff3d56
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-02-13 18:43:01 +0100

    Updated

--- src/app.py
+++ src/app.py
@@ -4,6 +4,10 @@ from config import config
 
 app=Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('auth/login.html')
+
 if __name__=='__main__':
     app.config.from_object(config['development'])
     app.run()
